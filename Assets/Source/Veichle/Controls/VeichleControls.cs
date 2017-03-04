3107fc18cbf0bbf6bc13b93e437e18622a7ffaa8
Menion93 <andrea.salvoni93@gmail.com>
2017-03-04 02:55:12 +0100

    the Veichle has now disabled input when the game ends, has also new base class for inputs

---
+++ Assets/Source/Veichle/Controls/VeichleControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class VeichleControls : MonoBehaviour {
+
+    protected VeichleSimulation veichle;
+
+    GameManager gameManager;
+
+    void Start()
+    {
+        veichle = GetComponentInChildren<VeichleSimulation>();
+        gameManager = GameObject.FindGameObjectWithTag(Tag.GameManager).GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(!gameManager.disableUserInput)
+        {
+            MyUpdate();
+        }
+    }
+
+    protected virtual void MyUpdate()
+    {
+
+    }
+}
