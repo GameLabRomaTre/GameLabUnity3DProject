ccd9602665a67281e9b94e6cacbd3c2acf902299
Menion93 <andrea.salvoni93@gmail.com>
2017-03-11 16:42:48 +0100

    now the camera watch the car from above and knows how to follow

---
+++ Assets/Source/Camera/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float offset;
+
+    public Transform veichle;
+    Transform myTrasform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myTrasform = GetComponent<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTrasform.position = new Vector3(veichle.position.x- veichle.forward.x * offset,
+                                              myTrasform.position.y,
+                                              veichle.position.z - veichle.forward.z * offset);
+
+        myTrasform.eulerAngles = new Vector3(myTrasform.eulerAngles.x,
+                                             veichle.eulerAngles.y,
+                                             myTrasform.eulerAngles.z);
+
+    }
+}
